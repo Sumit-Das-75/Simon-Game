7edec0430c24a0eae1fedb41f05c14f9b0faf2f5
Sumit-Das-75 <109627500+Sumit-Das-75@users.noreply.github.com>
2023-01-08 00:16:34 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,102 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern=[];
+
+var started = false; 
+var level=0;
+
+$(document).keypress(function()
+{
+  if(!started)
+  {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started=true;
+  }
+})
+
+function nextSequence() {
+
+  userClickedPattern=[];
+  level++;
+  $("#level-title").text("Level " + level);
+
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+$(".btn").click(function()
+{
+  
+  userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  check((userClickedPattern.length-1))
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour)
+
+})
+
+function check(checklLevel)
+{
+  if(userClickedPattern[checklLevel]===gamePattern[checklLevel])
+  {
+    if(userClickedPattern.length===gamePattern.length)
+    {
+      setTimeout(function()
+      {
+        nextSequence();
+      },1000)
+    }
+  }
+  else
+  {
+    faLse();
+  }
+}
+
+
+function faLse()
+{
+  var audio = new Audio("sounds/wrong.mp3");
+  audio.play();
+  $("body").addClass("game-over");
+  setTimeout(function()
+  {
+    $("body").removeClass("game-over");
+  },200);
+
+  $("#level-title").text("Game over, press any key to continue !!");
+
+  startOver();
+}
+
+function playSound(name)
+{
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function startOver()
+{
+  level=0;
+  gamePattern=[];
+  started=false;
+}
